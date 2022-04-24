c75fcf719153670283e6302354d0669aa219896d
Ivan Gordeev <ivgordeev@hotmail.com>
2022-04-24 17:17:55 +0300

    Выполнил задание "Картинки".

---
+++ Pictures/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Pictures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int picturesInRow = 3;
+            int allPictures = 52;
+
+            int fullRowPictures = allPictures / picturesInRow;
+            int picturesLeft = allPictures % picturesInRow;
+            Console.WriteLine($"Полностью заполненных рядов с картинками - {fullRowPictures}, картинок осталось: {picturesLeft}.");
+        }
+    }
+}

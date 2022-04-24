ef9dcc0dcb679be66b7f0c9e1a5ca49dfcb77127
Ivan Gordeev <ivgordeev@hotmail.com>
2022-04-24 16:21:28 +0300

    Выполнил задачу "Работа со строками".

---
+++ UserInfo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace UserInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите ваше имя: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Укажите ваш возраст: ");
+            string userAge = Console.ReadLine();
+            Console.WriteLine("Введите ваше любимое животное: ");
+            string animalKind = Console.ReadLine();
+            Console.WriteLine("Укажите ваш любимый музыкальный жанр: ");
+            string musicGenre = Console.ReadLine();
+            Console.WriteLine("Вас зовут " + userName + ", ваш возраст -  " + userAge + ", ваше любимое животное: " + animalKind + ", и вы слушаете " + musicGenre + ".");
+        }
+    }
+}

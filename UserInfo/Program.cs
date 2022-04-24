95b1cd27a12339e4e888ef62e173962472af1cf8
Ivan Gordeev <ivgordeev@hotmail.com>
2022-04-24 16:46:55 +0300

    Выполнил задание "Работа со строками".

--- UserInfo/Program.cs
+++ UserInfo/Program.cs
@@ -6,15 +6,15 @@ namespace UserInfo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите ваше имя: ");
+            Console.Write("Введите ваше имя: ");
             string userName = Console.ReadLine();
-            Console.WriteLine("Укажите ваш возраст: ");
-            string userAge = Console.ReadLine();
-            Console.WriteLine("Введите ваше любимое животное: ");
+            Console.Write("Укажите ваш возраст: ");
+            int userAge = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите ваше любимое животное: ");
             string animalKind = Console.ReadLine();
-            Console.WriteLine("Укажите ваш любимый музыкальный жанр: ");
+            Console.Write("Укажите ваш любимый музыкальный жанр: ");
             string musicGenre = Console.ReadLine();
-            Console.WriteLine("Вас зовут " + userName + ", ваш возраст -  " + userAge + ", ваше любимое животное: " + animalKind + ", и вы слушаете " + musicGenre + ".");
+            Console.Write($"Вас зовут {userName}, ваш возраст - {userAge}, ваше любимое животное: {animalKind} и вы слушаете {musicGenre}.");
         }
     }
 }

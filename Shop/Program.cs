2650c7e6af78edfd38d2430b4cc950659fd284a7
Ivan Gordeev <ivgordeev@hotmail.com>
2022-04-24 17:51:40 +0300

    так

---
+++ Shop/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вы пришли в магазин. Сколько золота у вас в кармане?");
+            int goldCount = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Привет, странник! Добро пожаловать в мой магазин.");
+        }
+    }
+}

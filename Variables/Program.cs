bb02a66df7b7cd0c9db5984de779fc88116957c0
Ivan Gordeev <ivgordeev@hotmail.com>
2022-04-24 15:47:44 +0300

    Выполнил первое задание

---
+++ Variables/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte monthsCount = 12;
+            sbyte currentLevel = 22;
+            short planeSpeed = 600;
+            int characterAge = 16;
+            long experienceCount = 1000000;
+            float itemPrice = 169.95f;
+            double characterWeight = 120.01;
+            char symbolU = 'U';
+            string petName = "Sharik";
+            bool isValid = true;
+
+        }
+    }
+}

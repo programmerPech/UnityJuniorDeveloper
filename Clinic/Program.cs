c765194bc43dd75e4c00fa71162112741b98ff3d
Ivan Gordeev <ivgordeev@hotmail.com>
2022-04-24 18:31:18 +0300

    Выполнил задание "Поликлиника".

---
+++ Clinic/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Clinic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Добро пожаловать в поликлинику. Вставайте в очередь! Сколько бабушек перед вами в очереди? ");
+            int grannyCount = Convert.ToInt32(Console.ReadLine());
+            int receptionTime = 10;
+            int waitingTime = grannyCount * receptionTime;
+            int waitingHours = waitingTime / 60;
+            int waitingMinutes = waitingTime % 60;
+            Console.WriteLine($"Перед вами в очереди {grannyCount} бабушек, время ожидания составляет {waitingHours} часа(ов) и {waitingMinutes} минут.");
+        }
+    }
+}

1ba2c6fe030b521ac9fd369048887028177e54b9
Ivan Gordeev <ivgordeev@hotmail.com>
2022-04-24 17:40:17 +0300

    Выполнил задание "Перестановка местами значений".

---
+++ SwapYourName/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SwapYourName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Doe";
+            string surname = "John";
+
+            string fullName = firstName + " " + surname;
+            string swapFullName = surname + " " + firstName;
+            Console.WriteLine($"Имя и фамилия до перестановки - {fullName}.");
+            Console.WriteLine($"Имя и фамилия после перестановки - {swapFullName}.");
+
+        }
+    }
+}

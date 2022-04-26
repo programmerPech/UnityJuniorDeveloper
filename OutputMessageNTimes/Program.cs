66771bd5314c47ec57f5a457f453ef57a154d73f
Ivan Gordeev <ivgordeev@hotmail.com>
2022-04-26 18:21:27 +0300

    Выполнил задание "Освоение циклов".

---
+++ OutputMessageNTimes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace OutputMessageNTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userMessage;
+            int timesCount;
+            Console.Write("Введите ваше сообщение: ");
+            userMessage = Console.ReadLine();
+            Console.Write("Введите количество повторов вашего сообщения: ");
+            timesCount = Convert.ToInt32(Console.ReadLine());
+
+            while (timesCount-- > 0)
+            {
+                Console.WriteLine(userMessage);
+            }
+        }
+    }
+}

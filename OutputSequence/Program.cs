4b4e0523e2dea005190f73287af7232705145f85
Ivan Gordeev <ivgordeev@hotmail.com>
2022-04-26 19:02:14 +0300

    Выполнил задание "Последовательность".

---
+++ OutputSequence/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace OutputSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int beginSequence = 5;
+            int endSequence = 96;
+            int stepSequence = 7;
+
+            for(int i = beginSequence; i<=endSequence; i+=stepSequence)
+            {
+                Console.Write(i+" ");
+            }
+        }
+    }
+}

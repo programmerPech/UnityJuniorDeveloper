9fd511a91376d66d880db0ea70b228cbe04820cc
Ivan Gordeev <ivgordeev@hotmail.com>
2022-05-02 21:09:58 +0300

    Выполнил задание "Скобочное выражение".

---
+++ BracketExpression/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace BracketExpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите ваше выражение со скобками: ");
+            string userBracketExpression = Console.ReadLine();
+            char bracketSymbolOpen = '(';
+            char bracketSymbolClose = ')';
+            int currentDeepBracket = 0;
+            int maxDeepBracket = 0;
+
+            for(int i = 0; i < userBracketExpression.Length; i++)
+            {
+
+                if(userBracketExpression[i]== bracketSymbolOpen)
+                {
+                    currentDeepBracket++;
+
+                    if(currentDeepBracket> maxDeepBracket)
+                    {
+                        maxDeepBracket = currentDeepBracket;
+                    }
+                }
+                else if(userBracketExpression[i] == bracketSymbolClose)
+                {
+                    currentDeepBracket--;
+                }
+            }
+
+            if (currentDeepBracket == 0)
+            {
+                Console.WriteLine($"Скобочное выражение {userBracketExpression} является корректным и имеет максимкльную глубину вложенности скобок - {maxDeepBracket}.");
+            }
+            else
+            {
+                Console.WriteLine("Скобочное выражение является НЕкорректным.");
+            }
+        }
+    }
+}

6a1621d01cb22c5b0c79cf7c401a398f7aea0547
Ivan Gordeev <ivgordeev@hotmail.com>
2022-05-02 11:40:44 +0300

    Выполнил задание "Вывод имени".

---
+++ OutputName/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace OutputName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstname;
+            char userSymbol;
+            Console.Write("Здравстуйте! Введите ваше имя: ");
+            firstname = Console.ReadLine();
+            Console.Write("Теперь введите ваш символ, в который следует заключить имя: ");
+            userSymbol = Convert.ToChar(Console.ReadLine());
+
+            for (int i = 0; i < firstname.Length; i++)
+            {
+                Console.Write(userSymbol+" ");
+            }
+
+            Console.WriteLine("\n"+userSymbol + " "+ firstname+ " " + userSymbol);
+
+            for (int i = 0; i < firstname.Length; i++)
+            {
+                Console.Write(userSymbol+" ");
+            }
+        }
+    }
+}

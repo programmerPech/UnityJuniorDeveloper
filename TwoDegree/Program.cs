3edfc92355c50379ddb3647f505c3ecb4e468b7f
Ivan Gordeev <ivgordeev@hotmail.com>
2022-05-02 17:37:58 +0300

    Выполнил задание "Степень двойки".

---
+++ TwoDegree/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TwoDegree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int beginRandom = 1;
+            int endRandom = 10000;
+            Random random = new Random();
+            int randomNumber = random.Next(beginRandom, endRandom);
+            Console.WriteLine("Заданное случайное число: " + randomNumber);
+            int twoDegree = 1;
+            int degree = 0;
+
+            while (randomNumber >= twoDegree)
+            {
+                twoDegree *= 2;
+                degree++;
+            }
+
+            Console.WriteLine($"заданное число {randomNumber}, степень {degree}, число в степени 2 {twoDegree}");
+        }
+    }
+}

d2f4c2be6d12e2d6b8d71f0b2c75de55e06b478c
Ivan Gordeev <ivgordeev@hotmail.com>
2022-05-02 17:10:26 +0300

    Выпонил задание "Кратные числа".

---
+++ MultipleNumbers/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace MultipleNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int beginNumberN = 1;
+            int endNumberN = 27;
+            int currentIndex = 0;
+            int beginThreeDigit = 100;
+            int endThreeDigit = 999;
+            int firstMultipleNumber=0;
+            int countNumber = 0;
+            Console.WriteLine("Введите N");
+            int numberN = int.Parse(Console.ReadLine());
+
+            if (beginNumberN <= numberN && numberN <= endNumberN)
+            {
+
+                for (int index = beginThreeDigit; index < endThreeDigit; index++)
+                {
+                    currentIndex = index;
+                    while (currentIndex >= 0)
+                    {
+
+                        if (currentIndex == 0)
+                        {
+                            firstMultipleNumber = index;
+                            countNumber++;
+                            break;
+                        }
+                        currentIndex -= numberN;
+                    }
+                }
+
+                while (firstMultipleNumber + numberN <= endThreeDigit)
+                {
+                    countNumber++;
+                    firstMultipleNumber = firstMultipleNumber + numberN;
+                }
+
+                Console.WriteLine($"Количество трехзначных чисел кратных {numberN} равняется {countNumber}. ");
+            }
+            else Console.WriteLine("Число N должно быть от 1 до 27.");
+        }
+    }
+}

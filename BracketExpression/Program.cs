6e3aa9c293d5310d46a4a7f44c6fa517f6882938
Ivan Gordeev <ivgordeev@hotmail.com>
2022-05-02 21:40:31 +0300

    Исправил задание "Скобочное выражение".

--- BracketExpression/Program.cs
+++ BracketExpression/Program.cs
@@ -24,7 +24,7 @@ namespace BracketExpression
                         maxDeepBracket = currentDeepBracket;
                     }
                 }
-                else if(userBracketExpression[i] == bracketSymbolClose && currentDeepBracket == 0)
+                else if(userBracketExpression[i] == bracketSymbolClose && currentDeepBracket > 0 )
                 {
                     currentDeepBracket--;
                 }

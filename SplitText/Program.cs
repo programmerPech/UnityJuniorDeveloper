270864f2b48142a66d9288b6ba2805256f5d32cc
Иван Гордеев <van_chezz@inbox.ru>
2022-05-07 09:00:23 +0300

    Выполнил задание "Split".

---
+++ SplitText/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SplitText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string textString = "Съешь ещё этих мягких французских булок да выпей чаю";
+            string[] wordArray = textString.Split(' ');
+
+            Console.WriteLine(textString);
+
+            foreach (string word in wordArray)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}

850e8b93a325c7332e235aed2193b5065d832a24
Иван Гордеев <van_chezz@inbox.ru>
2022-05-07 09:10:47 +0300

    Выполнил задание "Сдвиг значений массива".

---
+++ ShiftArrayValues/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ShiftArrayValues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int elementCount = 10;
+            int[] oneDimensionalArray = new int[elementCount];
+            Random random = new Random();
+            int beginRandomRange = 1;
+            int endRandomRange = 10;
+            int shiftCount = 0;
+
+            Console.WriteLine("Исходный массив: ");
+
+            for (int i = 0; i < oneDimensionalArray.Length; i++)
+            {
+                oneDimensionalArray[i] = random.Next(beginRandomRange, endRandomRange);
+                Console.Write(oneDimensionalArray[i]+" ");
+            }
+
+            Console.WriteLine("\nВведите число, на которое хотите сдвинуть массив влево: ");
+            shiftCount =Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < shiftCount; i++)
+            {
+                int firstElement = oneDimensionalArray[0];
+
+                for (int j = 1; j < elementCount; j++)
+                {
+                    oneDimensionalArray[j - 1] = oneDimensionalArray[j];
+                }
+
+                oneDimensionalArray[elementCount - 1] = firstElement;
+            }
+
+            Console.WriteLine("Сдвинутый массив:");
+
+            foreach (int element in oneDimensionalArray)
+            {
+                Console.Write(element+" ");
+            }
+        }
+    }
+}

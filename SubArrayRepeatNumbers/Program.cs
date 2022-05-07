c90a798805858ba4158d34e123d0b871056c72ab
Иван Гордеев <van_chezz@inbox.ru>
2022-05-07 08:43:48 +0300

    Выполнил задание "Подмассив повторений чисел".

---
+++ SubArrayRepeatNumbers/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace SubArrayRepeatNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int elementCount = 30;
+            int[] oneDimensionalArray = new int[elementCount];
+            int repeatNumber = oneDimensionalArray[0];
+            int repeatCount = 1;
+            int maxRepeatCount = 1;
+            Random random = new Random();
+            int beginRandomRange = 1;
+            int endRandomRange = 6;
+
+            Console.WriteLine("Исходный массив: ");
+
+            for (int i = 0; i < oneDimensionalArray.Length; i++)
+            {
+                oneDimensionalArray[i] = random.Next(beginRandomRange, endRandomRange);
+                Console.Write(oneDimensionalArray[i]+" ");
+            }
+
+            for (int i = 1; i < oneDimensionalArray.Length; i++)
+            {
+                if(oneDimensionalArray[i] == oneDimensionalArray[i - 1])
+                {
+                    repeatCount++;
+
+                    if (maxRepeatCount <= repeatCount)
+                    {
+                        repeatNumber = oneDimensionalArray[i];
+                        maxRepeatCount = repeatCount;
+                    }
+                }
+                else
+                {
+                    repeatCount = 1;
+                }
+            }
+
+            Console.WriteLine("\nПовторяющееся число: "+repeatNumber);
+            Console.WriteLine("Число повторений "+maxRepeatCount);
+        }
+    }
+}

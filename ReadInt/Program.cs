f329a80fe25690614d66b7f0f36f681c14eaa264
programmerPech <johnyjam2205@gmail.com>
2022-05-12 18:31:35 +0300

    Add files via upload

---
+++ ReadInt/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ReadInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RequestNumber();
+        }
+
+        static void RequestNumber()
+        {
+            string inputNumber = "";
+            int parseResult;
+
+            while(int.TryParse(inputNumber, out parseResult) != true)
+            {
+                Console.WriteLine("Введите число:");
+                inputNumber = Console.ReadLine();
+
+                if(int.TryParse(inputNumber, out parseResult) != true)
+                    Console.WriteLine("Ошибка! Ожидалось целое число.");
+            }
+
+            Console.WriteLine("Вы ввели число "+parseResult);
+        }
+    }
+}

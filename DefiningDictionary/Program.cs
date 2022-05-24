f0facf54c9e112122e4bc3736713aaec525eaa1c
programmerPech <johnyjam2205@gmail.com>
2022-05-24 09:22:06 +0300

    Add files via upload

---
+++ DefiningDictionary/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DefiningDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = "";
+            Dictionary<string, string> words = new Dictionary<string, string>();
+            words.Add("Инкапсуляция", "является одним из ключевых понятий объектно-ориентированного программирования и представляет сокрытие состояния объекта от прямого доступа извне.");
+            words.Add("Полиморфизм", "это свойство одних и тех же объектов и методов принимать разные формы.");
+            words.Add("Наследование", "является одним из ключевых моментов ООП. Благодаря наследованию один класс может унаследовать функциональность другого класса.");
+
+            while (userInput != "Выход")
+            {
+                Console.WriteLine("Введите слово, чтобы узнать его значение: ");
+                userInput = Console.ReadLine();
+                GetValue(words, userInput);
+            }
+        }
+
+        static void GetValue(Dictionary<string,string> words,string word)
+        {
+            switch (word)
+            {
+                case "Инкапсуляция":
+                    Console.WriteLine(words[word]);
+                    break;
+                case "Полиморфизм":
+                    Console.WriteLine(words[word]);
+                    break;
+                case "Наследование":
+                    Console.WriteLine(words[word]);
+                    break;
+                case "Выход":
+                    break;
+                default:
+                    Console.WriteLine("Такого слова нет.");
+                    break;
+            }
+        }
+    }
+}

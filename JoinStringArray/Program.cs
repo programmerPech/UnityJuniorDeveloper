ad01b47aede9598622ec3e34d78967df2722fb34
programmerPech <johnyjam2205@gmail.com>
2022-05-24 21:31:26 +0300

    Add files via upload

--- JoinStringArray/Program.cs
+++ JoinStringArray/Program.cs
@@ -7,20 +7,20 @@ namespace JoinStringArray
     {
         static void Main(string[] args)
         {
-            string[] numbers1 = new string[5] { "1", "2", "1", "2", "1" };
+            string[] numbers1 = new string[5] { "1", "2", "1", "2", "5" };
             string[] numbers2 = new string[5] { "4", "2", "1", "4", "3" };
-            List<string> newNumbers = new List<string>(0);
+            List<string> uniqueNumbers = new List<string>(0);
 
             Console.WriteLine("Массив строк номер один: ");
             ShowArray(numbers1);
             Console.WriteLine("\nМассив строк номер два: ");
             ShowArray(numbers2);
 
-            JoinArrays(newNumbers, numbers1);
-            JoinArrays(newNumbers, numbers2);
+            JoinArrays(uniqueNumbers, numbers1);
+            JoinArrays(uniqueNumbers, numbers2);
 
             Console.WriteLine("\nНовая коллекция");
-            ShowArray(newNumbers);
+            ShowArray(uniqueNumbers);
         }
 
         static void ShowArray(string[] array)
@@ -39,27 +39,27 @@ namespace JoinStringArray
             }
         }
 
-        static void JoinArrays(List<string> newNumbers, string[] numbers)
+        static void JoinArrays(List<string> uniqueNumbers, string[] numbers)
         {
-            if (newNumbers.Count == 0)
+            if (uniqueNumbers.Count == 0)
             {
-                newNumbers.Add(numbers[0]);
+                uniqueNumbers.Add(numbers[0]);
             }
 
             for (int i = 0; i < numbers.Length; i++)
             {
-                if (ContainElement(newNumbers, numbers[i]) == false)
+                if (ContainElement(uniqueNumbers, numbers[i]) == false)
                 {
-                    newNumbers.Add(numbers[i]);
+                    uniqueNumbers.Add(numbers[i]);
                 }
             }
         }
 
-        static bool ContainElement(List<string> newNumbers, string number)
+        static bool ContainElement(List<string> uniqueNumbers, string number)
         {
-            for (int i = 0; i < newNumbers.Count; i++)
+            for (int i = 0; i < uniqueNumbers.Count; i++)
             {
-                if (number == newNumbers[i])
+                if (number == uniqueNumbers[i])
                 {
                     return true;
                 }

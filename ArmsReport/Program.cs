f01a1581c20624f218c16b92d514ceddec1767b0
Ivan Gordeev <ivgordeev@hotmail.com>
2022-07-11 18:03:30 +0300

    Выполнил задание "Отчет о вооружении".

---
+++ ArmsReport/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ArmsReport
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Report report = new Report();
+            report.Show();
+        }
+    }
+    class Report
+    {
+        private List<Soldier> _soldiers = new List<Soldier>();
+
+        public Report()
+        {
+            _soldiers.Add(new Soldier("Семенов Максим","Штык-нож","Рядовой",6));
+            _soldiers.Add(new Soldier("Зайцев Никита","Автомат","Сержант", 10));
+            _soldiers.Add(new Soldier("Малышев Дмитрий","Пистолет","Ефрейтор",9));
+            _soldiers.Add(new Soldier("Кузнецов Даниил","Пулемет","Сержант",11));
+            _soldiers.Add(new Soldier("Горшков Иван","Пистолет","Рядовой",9));
+        }
+
+        public void Show()
+        {
+            var armsReport = from Soldier soldier in _soldiers
+                                select new
+                                {
+                                    name = soldier.Name,
+                                    rank = soldier.Rank
+                                };
+
+            foreach (var soldier in armsReport)
+            {
+                Console.WriteLine($"Имя: {soldier.name} \t Звание: {soldier.rank}");
+            }
+        }
+    }
+
+    class Soldier
+    {
+        private string _armament;
+        private int _lifeTime;
+        public string Name { get; private set; }
+        public string Rank { get; private set; }
+
+        public Soldier(string name, string armament, string rank, int lifeTime)
+        {
+            _armament = armament;
+            _lifeTime = lifeTime;
+            Name = name;
+            Rank = rank;
+        }
+    }
+}
